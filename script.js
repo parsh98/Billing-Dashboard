e6df53b6ceff57f1e7b178670e78e110592b9386
parsh98 <parasharradadiya@gmail.com>
2023-04-05 17:23:27 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+'use strict';
+
+function showSidebar() {
+  const x = document.querySelector('.div-1');
+  const y = document.querySelector('.menu-icon');
+
+  x.classList.toggle('show-div-1');
+  y.classList.toggle('move-menu-icon');
+}
